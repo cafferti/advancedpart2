93628aeb2308d199d6901455512ca09ff99825f2
cafferti <saulkumven@gmail.com>
2023-08-18 10:49:11 +0100

    created basic functions

---
+++ arrowfunc.js
@@ -0,0 +1,16 @@
+//types of functions
+ function regularfunc(){
+ return 8
+}
+
+const advancedfunc = function(){
+    return 6
+}
+
+const arrowfunc = () =>{
+ return 5
+}
+
+console.log(regularfunc())
+console.log(advancedfunc())
+console.log(arrowfunc())
